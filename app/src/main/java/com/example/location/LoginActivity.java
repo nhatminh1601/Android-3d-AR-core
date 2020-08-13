c1896737a9aaeb393c5150b735ce5b59c0e391d9
Ngô Tôn <ngovanton@asoview.vn>
2020-08-13 10:14:31 +0700

    Admin activity

--- app/src/main/java/com/example/location/LoginActivity.java
+++ app/src/main/java/com/example/location/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.location;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
