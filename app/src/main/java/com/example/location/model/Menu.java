4a70cc292df90be08cbbd4ab2bbdf3c93f182424
Ngô Tôn <ngovanton@asoview.vn>
2020-09-22 15:19:40 +0700

    Hotfix

---
+++ app/src/main/java/com/example/location/model/Menu.java
@@ -0,0 +1,60 @@
+package com.example.location.model;
+
+import java.io.Serializable;
+
+public class Menu implements Serializable {
+    int id;
+    String name, url, description;
+
+    public Menu() {
+    }
+
+    public Menu(int id, String name, String url, String description) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
